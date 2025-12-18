8e59a69ac507b9b62aa77cfd53872421a46cc39d
Tafzil Rimzal <tafsilrimzal18@gmail.com>
2025-12-18 15:49:44 +0700

    Add configuration file for database connection

---
+++ lab11_php_oop/assets/css/lab11_php_oop/config.php
@@ -0,0 +1,12 @@
+<?php
+
+$config = array(
+    'host'     => 'localhost',
+$config = [
+    'host' => 'localhost',
+    'username' => 'root',
+    'password' => '',
+    'db_name'  => 'latihan_oop'
+);
+    'db_name' => 'latihan_oop'
+];
